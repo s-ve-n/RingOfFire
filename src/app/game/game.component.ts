01daa80e0e0c36c63aede00ad4a76bea6656a552
plus <sven.radelof@gmail.com>
2022-02-05 23:46:08 +0100

    update

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,20 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.scss']
+  styleUrls: ['./game.component.scss'],
 })
 export class GameComponent implements OnInit {
   pickCardAnimation = false;
+  currentCard: string = '';
+  game!: Game;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
   }
 
   takeCard() {
-    this.pickCardAnimation = true;
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop()!;
+      this.pickCardAnimation = true;
+      console.log('New card:' + this.currentCard);
+      console.log('Game is', this.game);
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
+        this.pickCardAnimation = false;
+      }, 1000);
+    }
   }
-
 }
