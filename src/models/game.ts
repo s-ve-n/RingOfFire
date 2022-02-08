e6788fd668e4f9531abc9957017342fa87cd6a69
plus <sven.radelof@gmail.com>
2022-02-08 14:22:09 +0100

    update

--- src/models/game.ts
+++ src/models/game.ts
@@ -1,5 +1,5 @@
 export class Game {
-  public players: string[] = ['Player 1', 'Player 2', 'Player 3'];
+  public players: string[] = [];
   public stack: string[] = [];
   public playedCards: string[] = [];
   public currentPlayer: number = 0;
