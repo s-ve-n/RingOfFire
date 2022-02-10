e239bfbaaf2cd58af7f7b0d731727f15f2bf61bd
plus <sven.radelof@gmail.com>
2022-02-10 19:22:37 +0100

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAXgN2-XvUjoaDEOXjFQ7XBJJqKhcg-YyM',
+    authDomain: 'ring-of-fire-ca5af.firebaseapp.com',
+    databaseURL:
+      'https://ring-of-fire-ca5af-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'ring-of-fire-ca5af',
+    storageBucket: 'ring-of-fire-ca5af.appspot.com',
+    messagingSenderId: '890314378063',
+    appId: '1:890314378063:web:3e169c6415e0027334d33a',
+  },
 };
